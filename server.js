5aeed465dff4f85d238b75c0bd97c3e16a702096
Luis F Patrocinio <patrocinioluisf@gmail.com>
2024-12-06 14:58:29 -0300

    feat: changes aiming to get to work on vercel

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const express = require("express"); // Para criar o servidor
 const pug = require("pug"); // Para renderizar as páginas
 const axios = require("axios"); // Para fazer requisições à API
@@ -5,13 +7,19 @@ const axios = require("axios"); // Para fazer requisições à API
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Static Files
+app.use(express.static("public"));
+console.log("Static Files: OK");
+
 // Pug Setup
 app.set("view engine", "pug");
-app.set("views", "./views"); // Folder with the .pug files views
+app.set("views", "./views");
 
 // Home Page Route
 app.get("/", async (_req, res) => {
   try {
+    console.log("Carregando a página inicial...");
+
     // Request to the API to get the data
     const apiURL = "http://localhost:3001";
     const {
@@ -32,4 +40,4 @@ app.listen(port, () => {
   console.log(`PatroPage Server on port: ${port}`);
 });
 
-module.exports = app;
+// module.exports = app;
