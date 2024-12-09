fad3688b4f053db2ccd4c98f08cea4230f12461d
Luis F Patrocinio <patrocinioluisf@gmail.com>
2024-12-09 18:56:43 -0300

    feat: building...

---
+++ dist/controllers/loginController.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.loginPage = void 0;
+const apiURL = process.env.APIURL || "http://localhost:3001";
+function loginPage(req, res) {
+    try {
+        console.log("Carregando a página de login...");
+        res.render("login", { arcadeId: req.params.arcadeId });
+    }
+    catch (_err) {
+        console.error(_err);
+    }
+}
+exports.loginPage = loginPage;
--- dist/server.js
+++ dist/server.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const homeRoutes_1 = require("./routes/homeRoutes");
+const loginRoutes_1 = require("./routes/loginRoutes");
 require("dotenv").config();
 const express = require("express"); // Para criar o servidor
 const pug = require("pug"); // Para renderizar as páginas
@@ -15,6 +16,7 @@ app.set("views", path.join(__dirname, "../views"));
 app.use("/public", express.static(path.join(__dirname, "../public")));
 // Home Page Route
 app.use("/", homeRoutes_1.homeRoutes);
+app.use("/login", loginRoutes_1.loginRoutes);
 app.listen(port, () => {
     console.clear();
     console.log(`PatroPage Server on port: ${port}`);
