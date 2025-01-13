092f7f7fab5e9d906df68d6a4fea9a6c7178b1d4
Luis F Patrocinio <patrocinioluisf@gmail.com>
2025-01-12 22:22:23 -0300

    feat: Refactor project structure and update static assets paths.

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -1,69 +1,73 @@
-document.querySelector('#submitButton').addEventListener('click', function() {
-    const username = document.querySelector('#username').value;
-    const password = document.querySelector('#password').value;
-    const apiURL = "#{apiURL}"
+document.querySelector("#submitButton").addEventListener("click", function () {
+  const username = document.querySelector("#username").value;
+  const password = document.querySelector("#password").value;
+  const apiURL = "#{apiURL}";
 
-    const arcadeData = "#{arcadeData}" || null;
-    // Encerrar caso não tenhamos arcadeData
-    if (!arcadeData) {
-        return;
-    }
+  const arcadeData = "#{arcadeData}" || null;
+  // Encerrar caso não tenhamos arcadeData
+  if (!arcadeData) {
+    return;
+  }
 
-    const arcadeId = arcadeData.id;
-    console.log("Arcade ID: ", arcadeId);
+  const arcadeId = arcadeData.id;
+  console.log("Arcade ID: ", arcadeId);
 
-    fetch(`${apiURL}/login/${arcadeId}`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ username, password })
+  fetch(`${apiURL}/login/${arcadeId}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ username, password }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.type === "loginSuccess") {
+        window.location.href = "/player/" + data.content.id;
+      } else {
+        // TODO: Redirecionar para página de erro
+        alert("Falha ao logar: " + data.content);
+      }
     })
-    .then(response => response.json())
-    .then(data => {
-        if (data.type === 'loginSuccess') {
-            window.location.href = '/player/' + data.content.id;
-        } else {
-            // TODO: Redirecionar para página de erro
-            alert('Falha ao logar: ' + data.content);
-        }
-    })
-    .catch(error => {
-        console.error('Error:', error);
-        alert('An error occurred. Please try again.');
+    .catch((error) => {
+      console.error("Error:", error);
+      alert("An error occurred. Please try again.");
     });
 });
 
-document.querySelector('#adminSubmitButton').addEventListener('click', function() {
-    const username = document.querySelector('#username').value;
-    const password = document.querySelector('#password').value;
-    const apiURL = "#{apiURL}"
+document
+  .querySelector("#adminSubmitButton")
+  .addEventListener("click", function () {
+    const username = document.querySelector("#username").value;
+    const password = document.querySelector("#password").value;
+    const apiURL = "#{apiURL}";
 
     const arcadeTempId = "#{arcadeTempId}" || null;
     console.log("Temp ID: ", arcadeTempId);
     fetch(`${apiURL}/arcadeLogin/${arcadeTempId}`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ username, password })
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, password }),
     })
-    .then(response => response.json())
-    .then(data => {
-        if (data.type === 'loginSuccess') {
-            window.location.href = '/'; // TODO: Redirecionar para painel de admin.
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.type === "loginSuccess") {
+          window.location.href = "/"; // TODO: Redirecionar para painel de admin.
         } else {
-            alert("Falha ao logar: " + data.content);
+          alert("Falha ao logar: " + data.content);
         }
-    });
+      });
+  });
+
+document.addEventListener("DOMContentLoaded", function () {
+  if (arcadeTempId) {
+    document.querySelector("#adminSubmitButton").textContent =
+      "Entrar como Admin: " + arcadeTempId;
+    document.querySelector("#submitButton").style.display = "none";
+  }
+
+  if (!arcadeTempId) {
+    document.querySelector("#adminSubmitButton").style.display = "none";
+  }
 });
-
-const arcadeTempId = "#{arcadeTempId}" || null;
-if (arcadeTempId) {
-    document.querySelector('#adminSubmitButton').textContent = 'Entrar como Admin: ' + arcadeTempId;
-    document.querySelector('#submitButton').style.display = 'none';
-}
-
-if (!arcadeTempId) {
-    document.querySelector('#adminSubmitButton').style.display = 'none';
-}
--- src/server.ts
+++ src/server.ts
@@ -22,7 +22,7 @@ const port = process.env.PORT || 6969;
 // Pug Setup
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "../views"));
-app.use("/public", express.static(path.join(__dirname, "../public")));
+app.use(express.static(path.join(__dirname, "../public")));
 
 // Setup Routes
 app.use("/", homeRoutes);
